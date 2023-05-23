40619056e52a38a2207a8efd0c916a9bc584acb5
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-05-23 15:01:09 +0300

    added public static files from frontend build

--- server.js
+++ server.js
@@ -6,18 +6,28 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const cors = require('cors')
 
-// App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000',
-    ],
-    credentials: true,
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true,
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
-app.use(express.static('public'))
+
+// App Configuration
+// const corsOptions = {
+//     origin: [
+//         'http://127.0.0.1:8080',
+//         'http://localhost:8080',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3000',
+//     ],
+//     credentials: true,
+// }
+// app.use(cors(corsOptions))
+// app.use(express.static('public'))
 app.use(cookieParser()) // for res.cookies
 app.use(express.json()) // for req.body
 
@@ -156,4 +166,12 @@ app.get('/**', (req, res) => {
 })
 
 // Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
+
+const port = process.env.PORT || 3030
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+})
